51076842354409519747089b3b8516ec99ac3031
Daniil <Comm2k3@hotmail.com>
2022-12-17 14:01:47 +0200

    reverse array

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int[] GetArray(int minValue, int maxValue, int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int[] resultArray = GetArray(0, 10, 6);
+Console.WriteLine(String.Join("; ", resultArray));
+
+//1. Запись массива справо налево
+
+int[] ReverseArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    return result;
+}
+
+Console.WriteLine(String.Join("; ",ReverseArray(resultArray)));
